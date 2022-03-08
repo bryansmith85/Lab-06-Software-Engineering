db90582bdc249970cc737f16156f743c7220b26f
bryansmith85 <101148027+bryansmith85@users.noreply.github.com>
2022-03-07 19:37:16 -0500

    Number Difference Commit

--- VersionControl.java
+++ VersionControl.java
@@ -1,5 +1,6 @@
 /*  Program Name: Lab 06: Software Engineering
- *  Description: This is a program that will add two numbers.
+ *  Description: This is a program that will subtract 
+ *  one number from another number.
  *  Author: Bryan Smith
  *  Class Section: 10852
  *  Date: 03/07/2022
@@ -11,16 +12,16 @@ public class VersionControl {
     public static void main(String[] args) {
         int num1;
         int num2;
-        int sum;
+        int difference;
 
         Scanner scnr = new Scanner(System.in);
-        System.out.println("This is a program to add two numbers\n");
+        System.out.println("This is a program to subtract one number from another number.\n");
         System.out.println("Enter the first number: ");
         num1 = scnr.nextInt();
         System.out.println("Enter the second number: ");
         num2 = scnr.nextInt();
-        sum = num1 + num2;
-        System.out.println("The sum of the two numbers is: " + sum);
+        difference = num1 - num2;
+        System.out.println("The difference between the two numbers is: " + difference);
         scnr.close();
     }
 }
